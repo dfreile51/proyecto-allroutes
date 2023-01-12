2f97e0f0b2e6749190acf53c37eae3f6f136b255
Diego Freile <dfreile51@gmail.com>
2023-01-12 21:47:56 +0100

    Header y footer terminados

---
+++ layout/header.php
@@ -0,0 +1,10 @@
+<header>
+    <a href="#" class="logo">
+        <img src="imgs/excursionismo.png" alt="Logo" />
+        <span class="nombre-empresa">AllRoutes</span>
+    </a>
+    <nav class="botones">
+        <button class="nav-inicio">Iniciar sesión</button>
+        <button class="nav-registro"> Registrarse</button>
+    </nav>
+</header>
